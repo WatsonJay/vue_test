1d8bb5eba2ced6ef800f5aab0aed818d2eb14108
WatsonJay <446725026@qq.com>
2019-06-04 10:07:03 +0800

    Update Hello.vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -36,26 +36,29 @@
         </template>
       </el-table-column>
     </el-table>
-    <el-dialog title="信息" :visible.sync="dialogFormVisible" :before-close="handleClose">
-      <el-form :model="rowtemplate" :rules="rules" ref="infoform">
-        <el-form-item label="名字" :label-width="formLabelWidth"  prop="name">
-          <el-input size="medium" v-model="rowtemplate.name" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="地址" :label-width="formLabelWidth"  prop="address">
-          <el-select v-model="rowtemplate.address" placeholder="请选择地址">
-            <el-option label="上海市普陀区金沙江路 6666 弄" value="上海市普陀区金沙江路 6666 弄"></el-option>
-            <el-option label="上海市普陀区金沙江路 2333 弄" value="上海市普陀区金沙江路 2333 弄"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="日期" :label-width="formLabelWidth" prop="date">
+    <el-dialog title="信息" :visible.sync="dialogFormVisible" :before-close="handleClose" style="width: 60%;margin-left: auto;margin-right: auto;">
+      <div style="width: 90%;margin-left: auto;margin-right: auto;">
+        <el-form :model="rowtemplate" :rules="rules" ref="infoform">
+          <el-form-item label="名字:" :label-width="formLabelWidth"  prop="name">
+            <el-input size="medium" v-model="rowtemplate.name" autocomplete="off" style="width: 240px"></el-input>
+          </el-form-item>
+          <el-form-item label="地址:" :label-width="formLabelWidth"  prop="address">
+            <el-select v-model="rowtemplate.address" placeholder="请选择地址" style="width: 240px">
+              <el-option label="上海市普陀区金沙江路 6666 弄" value="上海市普陀区金沙江路 6666 弄"></el-option>
+              <el-option label="上海市普陀区金沙江路 2333 弄" value="上海市普陀区金沙江路 2333 弄"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="日期:" :label-width="formLabelWidth" prop="date" >
             <el-date-picker
               v-model="rowtemplate.date"
               type="date"
               placeholder="选择日期"
-              value-format="yyyy-MM-dd">
+              value-format="yyyy-MM-dd"
+              style="width: 240px">
             </el-date-picker>
-        </el-form-item>
-      </el-form>
+          </el-form-item>
+        </el-form>
+      </div>
       <div slot="footer" class="dialog-footer">
         <el-button @click="cancleEdit()">取 消</el-button>
         <el-button type="primary" @click="handleEdit('infoform')">确 定</el-button>
@@ -69,23 +72,27 @@
     data() {
       return {
         tableData: [{
+          Id: 1,
           date: '2016-05-02',
           name: '王小虎1',
           address: '上海市普陀区金沙江路 1518 弄'
         }, {
+          Id: 2,
           date: '2016-05-04',
           name: '王小虎2',
           address: '上海市普陀区金沙江路 1517 弄'
         }, {
+          Id: 3,
           date: '2016-05-01',
           name: '王小虎3',
           address: '上海市普陀区金沙江路 1519 弄'
         }, {
+          Id: 4,
           date: '2016-05-03',
           name: '王小虎4',
           address: '上海市普陀区金沙江路 1516 弄'
         }],
-        rowtemplate: {date: '', name: '', address: ''},
+        rowtemplate: {Id: 0, date: '', name: '', address: ''},
         rules:{
           name:[
             { required: true, message: '请输入姓名', trigger: 'blur' },
@@ -100,7 +107,7 @@
         },
         dialogFormVisible: false,
         search: '',
-        formLabelWidth: '200px'
+        formLabelWidth: '120px'
       }
     },
     methods: {
@@ -114,8 +121,12 @@
       handleEdit(formName){
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            this.tableData.push(this.rowtemplate)
-            this.rowtemplate = {date: '', name: '', address: ''}
+            if (this.rowtemplate.Id == 0) {
+              //设置当前新增行的Id
+              this.rowtemplate.Id = this.tableData.length + 1;
+              this.tableData.push(this.rowtemplate)
+            }
+            this.rowtemplate = {Id: 0, date: '', name: '', address: ''}
             this.dialogFormVisible = false
           } else {
             return false;
@@ -124,10 +135,10 @@
       },
       cancleEdit(){
         this.dialogFormVisible = false
-        this.rowtemplate={date: '', name: '', address: ''}
+        this.rowtemplate={Id: 0, date: '', name: '', address: ''}
       },
       handleClose(done){
-        this.rowtemplate={date: '', name: '', address: ''}
+        this.rowtemplate={Id: 0, date: '', name: '', address: ''}
         done();
       }
     },
